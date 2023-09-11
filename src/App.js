9d8591d23bbb6513bd81080e082dc64db4351acb
Kedar Sawant <kedar.sawant@nutrien.com>
2023-09-11 11:54:49 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
-import './App.css';
+import "./App.css";
 import Sidebar from "./Sidebar";
 import Map from "./Map";
-import {Machine} from "./Machine";
-import {useEffect, useState} from "react";
+import { Machine } from "./Machine";
+import { useEffect, useState } from "react";
 import axios from "axios";
+
 
 function App() {
   const [machineData, setMachineData] = useState([]);
   const [activeMachines, setActiveMachines] = useState([]);
-  const handleButtonClick = (machines: Machine[]) => {
+  const handleButtonClick = (machines) => {
     setActiveMachines(machines);
   };
   useEffect(() => {
-    axios.get('http://localhost:4000/machines')
-        .then(response => {
-          const data = response.data;
-          const mappedData = data.map((item) => {
-            return new Machine(item.id, item.vin, item.name, item.make, item.type, item.model, item.lastKnown, item.locationHistory);
-          });
-          setMachineData(mappedData);
-          setActiveMachines(mappedData);
-        })
-        .catch(error => {
-          console.error("Error Fetching From API", error)
+    axios
+      .get("http://localhost:4000/machines")
+      .then((response) => {
+        const data = response.data;
+        const mappedData = data.map((item) => {
+          return new Machine(
+            item.id,
+            item.vin,
+            item.name,
+            item.make,
+            item.type,
+            item.model,
+            item.lastKnown,
+            item.locationHistory
+          );
         });
+        setMachineData(mappedData);
+        setActiveMachines(mappedData);
+      })
+      .catch((error) => {
+        console.error("Error Fetching From API", error);
+      });
   }, []);
   return (
-      <div className="App">
-        <div className="sideBar">
-          {/* <Sidebar3 />*/}
-          <Sidebar machineList={machineData} onButtonClick={handleButtonClick}/>
-        </div>
-        <div className="map">
-          <Map activeMachines={activeMachines}/>
-        </div>
+    <div className="App">
+      <div className="sideBar">
+        {/* <Sidebar3 />*/}
+        <Sidebar machineList={machineData} onButtonClick={handleButtonClick} />
       </div>
+      <div className="map">
+        <Map activeMachines={activeMachines} />
+      </div>
+    </div>
   );
 }
-
 
 export default App;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,39 +1,56 @@
-import React, {useState} from 'react';
-import type {Machine} from "./Machine";
+import React, { useState } from "react";
+import logo from './assets/img/logo.png'
+// import type {Machine} from "./Machine";
 
-const Sidebar = ({machineList, onButtonClick}) => {
-    const [selectedButton, setSelectedButton] = useState('all');
-    const onButtonSelect = (buttonLabel) => {
-        setSelectedButton(buttonLabel);
-    }
+const Sidebar = ({ machineList, onButtonClick }) => {
+  const [selectedButton, setSelectedButton] = useState("all");
+  const onButtonSelect = (buttonLabel) => {
+    setSelectedButton(buttonLabel);
+  };
 
-    return (
-        <div>
-            <ul className="sidebar-links">
-                <li key='home'>
-                    <button id="all" className={selectedButton === 'all' ? "button-23-select" : "button-23"}  onClick={() => {
-                        onButtonSelect("all")
-                        onButtonClick(machineList)
-                    }}>All Machines
-                        <br/>
-                        <span className="org-name">Org Id: 157679</span>
-                    </button>
-                </li>
-            </ul>
-            <ul className="sidebar-links">
-                {
-                    machineList.map((machine) => (
-                        <li key={machine.id}>
-                            <button className={selectedButton === machine.vin ? "button-23-select" : "button-23"}
-                                    onClick={() => {
-                                        onButtonSelect(machine.vin)
-                                        onButtonClick([machine])}}>{machine.make + " " + machine.model}</button>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <div className="Logo-bg">
+        <img src={logo} height='100%' width="100%"></img>
+      </div>
+      <ul className="sidebar-links">
+        <li key="home">
+          <button
+            id="all"
+            className={
+              selectedButton === "all" ? "button-23-select" : "button-23"
+            }
+            onClick={() => {
+              onButtonSelect("all");
+              onButtonClick(machineList);
+            }}
+          >
+            All Machines
+            <br />
+            <span className="org-name">Org Id: 157679</span>
+          </button>
+        </li>
+      </ul>
+      <ul className="sidebar-links">
+        {machineList.map((machine) => (
+          <li key={machine.id}>
+            <button
+              className={
+                selectedButton === machine.vin
+                  ? "button-23-select"
+                  : "button-23"
+              }
+              onClick={() => {
+                onButtonSelect(machine.vin);
+                onButtonClick([machine]);
+              }}
+            >
+              {machine.make + " " + machine.model}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default Sidebar;
-
